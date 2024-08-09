ffb2eb61ad0afb914b04122a3be3fa1b08fa34c9
sangjun <byjun0302@gmail.com>
2024-08-09 17:41:37 +0900

    feat: add bottom btn to init_start_page

--- lib/src/init/page/init_start_page.dart
+++ lib/src/init/page/init_start_page.dart
@@ -1,5 +1,6 @@
 import 'package:chiikawamarketapp/src/common/app_font.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class InitStartPage extends StatelessWidget {
@@ -29,8 +30,31 @@ class InitStartPage extends StatelessWidget {
               align: TextAlign.center,
               size: 18,
               color: Colors.white.withOpacity(0.6),
-            )
+            ),
           ],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.only(
+          left: 25,
+          right: 25,
+          bottom: 25 + Get.mediaQuery.padding.bottom,
+        ),
+        child: GestureDetector(
+          onTap: () {},
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(7),
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              color: Colors.pink,
+              child: const AppFont(
+                '시작하기',
+                align: TextAlign.center,
+                size: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
         ),
       ),
     );
