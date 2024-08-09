8d66654dda84c098b6317b0063f253cd0d862f31
sangjun <byjun0302@gmail.com>
2024-08-09 13:48:09 +0900

    feat: add GetX routing to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:chiikawamarketapp/chiikawa_market.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    GetMaterialApp(
+      title: '치이카와 마켓',
+      initialRoute: '/',
       home: ChiikawaMarket(),
+      getPages: [
+        GetPage(name: '/', page: () => const ChiikawaMarket()),
+      ],
     ),
   );
 }
