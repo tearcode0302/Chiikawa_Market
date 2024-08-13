d3a96f0bca8b44fb1c36193ba7ce19f4a5ecfdf3
sangjun <byjun0302@gmail.com>
2024-08-13 13:57:54 +0900

    feat: add UserModel

---
+++ lib/src/user/user/user_model.dart
@@ -0,0 +1,17 @@
+import 'package:equatable/equatable.dart';
+
+class UserModel extends Equatable {
+  final String? uid;
+  final String? name;
+  final String? email;
+
+  const UserModel({
+    this.uid,
+    this.name,
+    this.email,
+  });
+
+  @override
+  List<Object?> get props => throw UnimplementedError();
+
+}
